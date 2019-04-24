cf71cb843f2871ec1474e8ae7f854306ecb49447
THEWALKINGDEAD <317576098@qq.com>
2019-04-24 18:10:08 +0800

    Update  code.

---
+++ punctuallyTime/punctuallyTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  punctuallyTime
+//
+//  Created by THEWALKINGDEAD on 2019/4/24.
+//  Copyright © 2019 THEWALKINGDEAD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ punctuallyTime/punctuallyTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  punctuallyTime
+//
+//  Created by THEWALKINGDEAD on 2019/4/24.
+//  Copyright © 2019 THEWALKINGDEAD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
